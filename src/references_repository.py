c3016a6db84de4bc56be5fc0835eedf9fd1e3ae2
Kangas Jyri V <jyri_kangas@outlook.com>
2022-11-25 16:33:16 +0200

    korjaus get_references metodiin

--- src/references_repository.py
+++ src/references_repository.py
@@ -1,11 +1,13 @@
 from database import get_db_connection
 
-database_connection = get_db_connection()
 
 ##palauttaa kaikki viitteet jossain muodossa
 def get_references():
+    database_connection = get_db_connection()
     cursor = database_connection.cursor()
-    references = cursor.execute("SELECT * FROM book;")
+
+    cursor.execute("SELECT * FROM book;")
+    references = cursor.fetchall()
     return references
 
 ##tänne metodi jolla voi lisätä viitteitä
