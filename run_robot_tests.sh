c95a1b5fba6c2d3e6646fdc9b095f0e93881127a
jyrikangas <55104869+jyrikangas@users.noreply.github.com>
2022-11-28 21:21:37 +0200

    Update run_robot_tests.sh

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -6,7 +6,6 @@ poetry run flask run &
 
 # odetetaan, että palvelin on valmiina ottamaan vastaan pyyntöjä
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5000)" != "200" ]];
-  echo 1
   do sleep 1;
 done
 
