3ab84b7f9453462aa86873a91bbd623016b73d11
Lotta Hyppyrä <lotta@hyppyra.net>
2022-12-13 09:44:41 +0200

    add refname

--- src/tests/test_references_repository.py
+++ src/tests/test_references_repository.py
@@ -5,17 +5,17 @@ from database import get_db_connection
 
 class TestReferencesRepository(unittest.TestCase):
     def setUp(self):
-        self.data = [('Martin, Robert', 'Clean Code: A Handbook of Agile Software Craftsmanship', 2008, 'Prentice Hall', 'computer science')]
+        self.data = [('Martin, Robert', 'Clean Code: A Handbook of Agile Software Craftsmanship', 2008, 'Prentice Hall', 'computer science', 'CleanCode')]
         self.conn = get_db_connection(test=True)
 
         # Create a test db
         cur = self.conn.cursor()
         cur.execute("DROP TABLE IF EXISTS book")
         cur.execute(
-            "CREATE TABLE book (id INTEGER PRIMARY KEY, author TEXT, title TEXT, year INTEGER, publisher TEXT, addedDate timestamp, tag TEXT)"
+            "CREATE TABLE book (id INTEGER PRIMARY KEY, author TEXT, title TEXT, year INTEGER, publisher TEXT, addedDate timestamp, tag TEXT, refname TEXT)"
             )
         cur.execute(
-            "INSERT INTO book (author, title, year, publisher, tag) VALUES ('Martin, Robert', 'Clean Code: A Handbook of Agile Software Craftsmanship', 2008, 'Prentice Hall', 'computer science')"
+            "INSERT INTO book (author, title, year, publisher, tag, refname) VALUES ('Martin, Robert', 'Clean Code: A Handbook of Agile Software Craftsmanship', 2008, 'Prentice Hall', 'computer science', 'CleanCode')"
             )
         self.conn.commit()
 
@@ -34,34 +34,34 @@ class TestReferencesRepository(unittest.TestCase):
 
     def test_add_book_with_correct_input(self):
         add_book('TestEst', 'TEsting is fun', 2000,
-                 'Tester publishing', 'test', self.conn)
+                 'Tester publishing', 'test', 'test', self.conn)
         references = get_references(self.conn)
         self.assertTrue(len(references) == 2)
 
     def test_add_book_with_wrong_length(self):
         added = add_book('', 'TEsting is fun', 2000,
-                         'Tester publishing', 'test', self.conn)
+                         'Tester publishing', 'test', 'test', self.conn)
 
         self.assertEqual(added, 'Syötteen pituus on oltava yli 1')
 
     def test_add_book_with_wrong_types(self):
         added = add_book(1000, 'TEsting is fun', 2000,
-                         'Tester publishing', 'test', self.conn)
+                         'Tester publishing', 'test', 'test', self.conn)
 
         self.assertEqual(added, 'Kirjailijan on oltava merkkijono')
 
         added = add_book('test', 1222, 2000,
-                         'Tester publishing', 'test', self.conn)
+                         'Tester publishing', 'test', 'test', self.conn)
 
         self.assertEqual(added, 'Otsikon on oltava merkkijono')
 
         added = add_book('test', 'test test', '2000',
-                         'Tester publishing', 'test', self.conn)
+                         'Tester publishing', 'test', 'test',self.conn)
 
         self.assertEqual(added, 'Vuoden on oltava numero')
 
         added = add_book('test', 'test test', 2000,
-                         1222, 'test', self.conn)
+                         1222, 'test', 'test',self.conn)
 
         self.assertEqual(added, 'Julkaisijan on oltava merkkijono')
 
@@ -78,7 +78,7 @@ class TestReferencesRepository(unittest.TestCase):
     def test_generate_bibtex(self):
         viite = get_reference_by_id('1', self.conn)
         bibtex = generate_bibtex(viite)
-        correct_bibtex = """@book{viite,\n author = {Martin, Robert},\n publisher = {Prentice Hall},\n title = {Clean Code: A Handbook of Agile Software Craftsmanship},\n year = {2008}\n}\n"""
+        correct_bibtex = """@book{CleanCode,\n author = {Martin, Robert},\n publisher = {Prentice Hall},\n title = {Clean Code: A Handbook of Agile Software Craftsmanship},\n year = {2008}\n}\n"""
         print(bibtex)
         print(correct_bibtex)
         self.assertEqual(bibtex, correct_bibtex)
